e92f4958751c8b721e94286cf59faad8608035c7
AndrewShmig <andrewshmig@gmail.com>
2013-03-10 18:02:45 +0400

    init

---
+++ ASADeepDictionary/ASADeepDictionary/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ASADeepDictionary
+//
+//  Created by AndrewShmig on 3/10/13.
+//  Copyright (c) 2013 AndrewShmig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
