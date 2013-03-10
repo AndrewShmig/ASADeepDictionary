19f99f0bc5e289b86cb73da04f359709f36a9a0d
AndrewShmig <andrewshmig@gmail.com>
2013-03-10 18:04:20 +0400

    Cleaning

--- ASADeepDictionary/ASADeepDictionary/main.m
+++ ASADeepDictionary/ASADeepDictionary/main.m
@@ -10,13 +10,12 @@
 
 int main(int argc, const char * argv[])
 {
-
   @autoreleasepool {
-      
-      // insert code here...
-      NSLog(@"Hello, World!");
-      
+   
+    
+    
+    
   }
-    return 0;
+  return 0;
 }
 
